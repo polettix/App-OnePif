bc5d19ce2048372b01a2becfbbf96a6db29c3ec6
Flavio Poletti <flavio@polettix.it>
2017-01-03 16:14:57 +0100

    Fix implementation (from previous scripts)

--- lib/App/OnePif.pm
+++ lib/App/OnePif.pm
@@ -4,9 +4,35 @@ use warnings;
 { our $VERSION = '0.001'; }
 use English qw( -no_match_vars );
 use Carp;
+use 5.010;
 
 use Mo qw< is default >;
 use Path::Tiny;
+use YAML::Tiny;
+
+{
+   no warnings 'redefine';
+   my @booleans = (
+      'JSON::PP::Boolean',
+      'JSON::XS::Boolean',
+      'Types::Serialiser::Boolean',  # should not be needed
+      'Mojo::JSON::_Bool',           # only up to Mojolicious 6.21
+      # Dancer, Dancer2 use JSON
+   );
+   sub YAML::Tiny::dumper_for_unknown {
+      my ($self, $element, $line, $indent, $seen) = @_;
+      my $type = ref $element;
+
+      for my $boolean (@booleans) {
+         next unless $element->isa($boolean);
+         $line .= $element ? ' true' : ' false';
+         return $line;
+      }
+
+      # no known boolean... complain!
+      die \"YAML::Tiny does not support $type references";
+   }
+}
 
 has file => (
    is      => 'rw',
@@ -19,36 +45,28 @@ has attachments_dir => (
    lazy => 1,
    default => sub {
       my $self = shift;
-      Path::Class::file($self->file())->dir()->subdir('attachments')
+      path($self->file())->sibling('attachments')
    },
 );
 
-has records => (
-   is      => 'rw',
-   lazy    => 1,
+has records_byid => (
+   is => 'rw',
+   lazy => 1,
    default => sub {
-      my ($self) = @_;
-      my $file = $self->file();
-      open my $fh, '<:raw', $file
-        or croak "open('$file'): $OS_ERROR";
-      my $decoder = $self->JSON_decoder();
-      my @records;
-      while (<$fh>) {
-         my $record = $decoder->($_);
-         if (my $attachments = $self->attachments_for($record->{uuid})) {
-            $record->{attachments} = $attachments;
-         }
-         push @records, $record;
-         scalar <$fh>;    # drop a line
+      my $self = shift;
+      my $bt = $self->records_bytype;
+      my %retval;
+      for my $list (values %$bt) {
+         $retval{$_->{_id}} = $_ for @$list;
       }
-      return \@records;
+      return \%retval;
    },
 );
 
 has records_bytype => (
    is => 'rw',
    lazy => 1,
-   default => sub { $_[0]->_records_by('typeName', 'title') },
+   default => \&DEFAULT_records,
 );
 
 has JSON_decoder => (
@@ -109,6 +127,12 @@ has type => (
    default => sub { '*' },
 );
 
+has types => (
+   is => 'rw',
+   lazy => 1,
+   default => \&DEFAULT_types,
+);
+
 sub run {
    my ($package, @ARGV) = @_;
    $package->new(args => \@ARGV)->run_interactive();
@@ -116,12 +140,28 @@ sub run {
 
 sub run_interactive {
    my ($self) = @_;
+   my %main_for = (
+      '.q' => 'quit',
+      e => 'exit',
+      f => 'file',
+      h => 'help',
+      l => 'list',
+      p => 'print',
+      q => 'quit',
+      s => 'search',
+      t => 'type',
+      ts => 'types',
+      u => 'type',
+      use => 'type',
+   );
    require Term::ReadLine;
    my $term = $self->term();
    my $out  = $self->out();
+   $self->records_bytype; # just load...
    while (defined(my $line = $term->readline('1password> '))) {
       my ($command, $rest) = $line =~ m{\A \s* (\S+) \s* (.*?) \s*\z}mxs;
       next unless defined($command) && length($command);
+      $command = $main_for{$command} if exists $main_for{$command};
       if (my $cb = $self->can("do_$command")) {
          $self->$cb($rest);
       }
@@ -133,20 +173,17 @@ sub run_interactive {
 
 sub attachments_for {
    my ($self, $uuid) = @_;
-   my $target = $self->attachments_dir()->subdir($uuid);
-   return unless -e $target;
-   return [ map { $_->stringify() } $target->children() ];
+   my $target = $self->attachments_dir()->child($uuid);
+   return unless $target->exists;
+   return [ map { $_->stringify } $target->children ];
 }
 
 sub clear_records {
    my ($self) = @_;
-   delete $self->{records};
-   return $self;
-}
-
-sub clear_attachments_dir {
-   my ($self) = @_;
-   delete $self->{attachments_dir};
+   delete $self->{records_bytype};
+   delete $self->{records_byid};
+   delete $self->{types};
+   $self->{type} = '*';
    return $self;
 }
 
@@ -154,29 +191,32 @@ sub do_help {
    my ($self) = @_;
    $self->print(<<'END_OF_HELP');
 Available commands:
-* quit
+* quit (also: q, .q)
    exit the program immediately, exit code is 0
-* exit [code]
+* exit [code] (also: e)
    exit the program immediately, can accept optional exit code
-* file [filename]
+* file [filename] (also: f)
    set the filename to use for taking data (default: 'data1.pif')
-* types
+* types (also: ts)
    show available types and possible aliases
-* type [wanted]
+* type [wanted] (also: t, use, u)
    get current default type or set it to wanted. It is possible to
    reset the default type by setting type "*" (no quotes)
-* use [wanted]
-   alias for type
-* list [type]
+* list [type] (also: l)
    get a list for the current set type. By default no type is set
    and the list includes all elements, otherwise it is filtered
    by the wanted type.
    If type parameter is provided, work on specified type instead
    of default one.
-* print [ <id> | <type> <id> ]
+* print [ <id> ] (also: p)
    show record by provided id (look for ids with the list command).
    It is also possible to specify the type, in which case the id
    is interpreted in the context of the specific type.
+* search <query-string> (also: s)
+   search for the query-string, literally. Looks for a substring in
+   the YAML rendition of each record that is equal to the query-string,
+   case-insensitively. If a type is set, the search is restricted to
+   that type.
 END_OF_HELP
 }
 
@@ -204,65 +244,52 @@ sub do_file {
    return $self;
 } ## end sub do_file
 
-sub has_type {
-   my ($self) = @_;
-   return exists $self->{type};
+sub DEFAULT_types {
+   my $self = shift;
+
+   state $aliases_for = {
+      'passwords.Password' => [qw< p password passwords >],
+      'securenotes.SecureNote' => [qw< note notes >],
+      'wallet.computer.License' => [qw< license licenses >],
+      'webforms.WebForm' => [qw< form forms >],
+      'wallet.financial.CreditCard' => [qw< card cards >],
+   };
+
+   my $rbt = $self->records_bytype;
+   my %retval = ('*' => ['*', '*']);
+   for my $type (keys %$rbt) {
+      my @alternatives = ($type, sort {
+         (length($a) <=> length($b)) || ($a cmp $b)
+      } @{$aliases_for->{$type} // []});
+      push @alternatives, $type;
+      $retval{$_} = \@alternatives for @alternatives;
+   }
+
+   # now first item is always the canonical form, second is the shortest
+   # alias, then the rest including the canonical form at the end
+   return \%retval;
 }
-
-sub clear_type {
-   my ($self) = @_;
-   delete $self->{type};
-   return $self;
-}
-
-my %type_for = (
-   'passwords.Password'          => 'passwords.Password',
-   'securenotes.SecureNote'      => 'securenotes.SecureNote',
-   'wallet.computer.License'     => 'wallet.computer.License',
-   'webforms.WebForm'            => 'webforms.WebForm',
-   'wallet.financial.CreditCard' => 'wallet.financial.CreditCard',
-   password                      => 'passwords.Password',
-   note                          => 'securenotes.SecureNote',
-   license                       => 'wallet.computer.License',
-   form                          => 'webforms.WebForm',
-   ccard                         => 'wallet.financial.CreditCard',
-   passwords                     => 'passwords.Password',
-   notes                         => 'securenotes.SecureNote',
-   licenses                      => 'wallet.computer.License',
-   forms                         => 'webforms.WebForm',
-   ccards                        => 'wallet.financial.CreditCard',
-   '*' => '*',
-);
-
-my %type_aliases_for;
 
 sub do_types {
    my ($self) = @_;
-   if (! keys %type_aliases_for) {
-      while (my ($k, $v) = each %type_for) {
-         my @stuff = sort {
-            length($a) <=> length($b)
-         } (@{$type_aliases_for{$v} || []}, $k);
-         $type_aliases_for{$v} = \@stuff;
-      }
-   }
-   my (%mix);
-   my $current = $self->type();
+
+   # might cache this somewhere...
+   my %shorts;
    my $length = 0;
-   for my $type (keys %{$self->records_bytype()}) {
-      next unless exists $type_aliases_for{$type};
-      my ($shorter, @aliases) = @{$type_aliases_for{$type}};
-      $current = $shorter if $type eq $current;
-      $length = length($shorter)
-         if length($shorter) > $length;
-      $mix{$shorter} = \@aliases;
+   for my $list (values %{$self->types}) {
+      my ($canon, $shorter, @rest) = @$list;
+      $shorts{$shorter} = \@rest;
+      $length = length($shorter) if length($shorter) > $length;
    }
-   $mix{'*'} = ' (accept any type)';
+   $shorts{'*'} = ' (accept any type)';
+
+   my $current = $self->type;
    my $marker = '<*>';
    my $blanks = ' ' x length $marker;
-   for my $type (sort(keys %mix)) {
-      my $rest = $mix{$type};
-      $rest = " (also: @$rest)" if ref $rest;
+   for my $type (sort(keys %shorts)) {
+      my $rest = $shorts{$type};
+      $rest = " (also: @$rest)" if ref($rest) && @$rest;
+      $rest = '' if ref $rest;
       my $indicator = $type eq $current ? $marker : $blanks;
       $self->print(sprintf "%s %${length}s%s", $indicator, $type, $rest);
    }
@@ -270,13 +297,16 @@ sub do_types {
 
 sub real_type {
    my ($self, $type) = @_;
-   return $type_for{$type || $self->type()};
+   return '*' unless defined $type;
+   my $types = $self->types;
+   return unless exists $types->{$type};
+   return $types->{$type}[0];
 }
 
 sub do_type {
    my ($self, $type) = @_;
    if (defined $type && length $type) {
-      if (exists $type_for{$type}) {
+      if ($self->real_type($type)) {
          $self->type($type);
       }
       else {
@@ -288,33 +318,39 @@ sub do_type {
    }
 } ## end sub do_type
 
-sub do_use {
-   my $self = shift;
-   $self->do_type(@_);
-}
-
 sub print {
    my $self = shift;
    print {$self->out()} @_, "\n";
 }
 
-sub _records_by {
-   my ($self, $field, $sorter) = @_;
-   my %retval;
-   for my $record (@{$self->records()}) {
-      my $key = $record->{$field};
-      push @{$retval{$key}}, $record;
-   }
-   if ($sorter) {
-      if (! ref $sorter) {
-         my $sf = $sorter;
-         $sorter = sub { $a->{$sf} cmp $b->{$sf} };
+sub DEFAULT_records {
+   my ($self) = @_;
+   my $file = $self->file();
+   open my $fh, '<:raw', $file
+      or croak "open('$file'): $OS_ERROR";
+   my $decoder = $self->JSON_decoder;
+   my %by_type;
+   while (<$fh>) {
+      my $record = $decoder->($_);
+      if (my $attachments = $self->attachments_for($record->{uuid})) {
+         $record->{attachments} = $attachments;
       }
-      for my $list (values %retval) {
-         @$list = sort { &$sorter } @$list;
-      }
+      push @{$by_type{$record->{typeName}}}, $record;
+      scalar <$fh>;    # drop a line
    }
-   return \%retval;
+
+   for my $list (values %by_type) {
+      @$list = sort { $a->{title} cmp $b->{title} } @$list;
+   }
+
+   my $dumper  = $self->YAML_dumper;
+   _traverse(\%by_type, undef, sub {
+      my ($n, $v) = @_;
+      $v->{_id} = $n;
+      $v->{_yaml} = $dumper->($v);
+   });
+
+   return \%by_type;
 }
 
 sub do_list {
@@ -324,24 +360,46 @@ sub do_list {
    my $records = $self->clipped_records_bytype($type);
    _traverse($records, sub {
       my ($key) = @_;
-      $self->print($key) if $type eq '*'; 
+      $self->print($key) if $type eq '*';
    }, sub {
       my ($n, $record) = @_;
-      $self->print(sprintf('   %3d %s', $n, $record->{title}));
+      $self->print(sprintf('   %3d %s', $record->{_id}, $record->{title}));
    });
 }
 
 sub do_print {
-   my ($self, $rest) = @_;
-   my ($type, $n) = split /\s+/, $rest;
-   ($type, $n) = ($n, $type) unless defined($n) && $n =~ m{\A\d+\z};
+   my ($self, $id) = @_;
+   my $by_id = $self->records_byid;
+   if ($id && exists($by_id->{$id})) {
+      $self->print($by_id->{$id}{_yaml});
+   }
+   else {
+      $self->print('invalid id');
+   }
+}
+
+sub do_search {
+   my ($self, $query) = @_;
+   $query = '' unless defined $query;
+   $query =~ s{\A\s+|\s+\z}{}gmxs;
+   return $self->do_list unless length $query;
+
+   $query = quotemeta $query; # ready for a regex now
+   my $type = $self->real_type($self->type);
    my $records = $self->clipped_records_bytype($type);
-   my $target;
+   my $last_printed_type = $type;
    _traverse($records, undef, sub {
-      my ($i, $record) = @_;
-      $target = $record if $n == $i;
-   });
-   $self->print($self->YAML_dumper()->($target));
+         my ($n, $record) = @_;
+         if ($record->{_yaml} =~ m{$query}i) {
+            my $rt = $self->real_type($record->{typeName});
+            if ($last_printed_type ne $rt) {
+               $self->print($record->{typeName});
+               $last_printed_type = $rt;
+            }
+            $self->print(sprintf('   %3d %s', $record->{_id},
+                  $record->{title}));
+         }
+      });
 }
 
 sub clipped_records_bytype {
